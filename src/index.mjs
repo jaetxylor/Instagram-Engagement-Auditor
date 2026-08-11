3982d93a77609c3e87795260462f7fd439439dc3
Jeremiah Leow <jeremiah.leow@psrmalam.com>
2026-08-11 10:37:04 +0800

    feat: expose shared product layer

--- src/index.mjs
+++ src/index.mjs
@@ -3,3 +3,4 @@ export * as connectors from "./connectors/index.mjs";
 export * as runtime from "./runtime/index.mjs";
 export * as storage from "./storage/index.mjs";
 export * as browser from "./browser/index.mjs";
+export * as product from "./product/index.mjs";
